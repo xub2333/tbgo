09e537bb70e6d4c35de193b9999f9222f0efaff2
xub2333 <miss.xu.bin@gmail.com>
2020-03-08 14:19:44 +0800

    tbgo code

---
+++ src/main/java/tbgojar/controller.java
@@ -0,0 +1,21 @@
+package tbgojar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tbgo")
+@EnableAutoConfiguration
+public class controller {
+	@GetMapping("login")
+	public String show() {
+		return "show login";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(controller.class, args);
+	}
+}
